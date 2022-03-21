b2f92f0418bf07998427c065e4bd82813f012e59
lysmm203 <lyeongsuk97@gmail.com>
2022-03-20 23:03:58 -0400

    Added all required functionalities

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,15 +6,27 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+
+        // --- Copy this only
+        
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "qPU6herYlfSuSkgNRvmw2NVpWfDgk3AmLAVlE93X" // <- UPDATE
+                $0.clientKey = "h2egbljUxWeysn8pStUw8xyD3QNGJUjYzbUpRLpa" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
+        
+        // --- end copy
         return true
+        
     }
 
     // MARK: UISceneSession Lifecycle
@@ -30,6 +42,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+    
+
 
 
 }
